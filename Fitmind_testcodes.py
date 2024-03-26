2693321b677a280f5404ffb53df2aa52dec39445
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-03-26 13:26:23 +0100

    Update Fitmind_testcodes.py

--- Fitmind_testcodes.py
+++ Fitmind_testcodes.py
@@ -4,9 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-
-st.sidebar.title("Menu")
-
 st.sidebar.header("Categories")
 
 page = st.sidebar.radio("Choose what you need",["FitMind - Introduction", "Fitness", "Mental Health", "Food & Recipes"])
@@ -24,13 +21,13 @@ if page == "Fitness":
         st.write("Content for Flexibility category")
     elif selected_subcategory == "Endurance":
         st.write("Content for Endurance category")
-
-st.sidebar.subheader("Second Subcategory")
+    
+    st.sidebar.subheader("Second Subcategory")
     second_subcategory = st.sidebar.selectbox("Choose a second subcategory", ["Option 1", "Option 2", "Option 3"])
     
-if second_subcategory == "Option 1":
-    st.write("Content for Option 1")
-elif second_subcategory == "Option 2":
-    st.write("Content for Option 2")
-elif second_subcategory == "Option 3":
-    st.write("Content for Option 3")
+    if second_subcategory == "Option 1":
+        st.write("Content for Option 1")
+    elif second_subcategory == "Option 2":
+        st.write("Content for Option 2")
+    elif second_subcategory == "Option 3":
+        st.write("Content for Option 3")
