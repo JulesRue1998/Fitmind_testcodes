be77c8096681b28e28f581914c1ed41ab57ae81d
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-05-20 22:58:01 +0200

    Update Fitmind_testcodes.py

--- Fitmind_testcodes.py
+++ Fitmind_testcodes.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Festlegung der Anzahl der Zeilen im DataFrame
 num_rows = 1
 
 # Erstellung eines leeren DataFrames mit den Spalten "Stimmung" und "Stress"
-df = pd.DataFrame(columns=["Stimmung", "Stress"])
+df = pd.DataFrame(columns=["Datum", "Stimmung", "Stress"])
 
 # Heutiges Datum
 current_date = datetime.now().date()
@@ -17,7 +17,13 @@ for i in range(num_rows):
     mood = st.number_input(f"Stimmung", min_value=1, max_value=10, step=1)
     stress = st.number_input(f"Stress", min_value=1, max_value=10, step=1)
     # Hinzufügen der eingegebenen Daten als Zeile zum DataFrame
-    df.loc[current_date] = [mood, stress]
+    df.loc[current_date] = [current_date, mood, stress]
+
+# Filtern der Daten der letzten 30 Tage
+last_30_days = df[df['Datum'] >= current_date - timedelta(days=30)]
 
 # Anzeigen des erstellten DataFrames
 st.dataframe(df)
+
+# Line-Chart für die Daten der letzten 30 Tage anzeigen
+st.line_chart(last_30_days.set_index('Datum')[['Stimmung', 'Stress']])
