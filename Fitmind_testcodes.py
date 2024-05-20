f9fbcb7e1db4074b246ebe4725c1972f3225f588
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-05-20 22:38:34 +0200

    Update Fitmind_testcodes.py

--- Fitmind_testcodes.py
+++ Fitmind_testcodes.py
@@ -5,11 +5,11 @@ import pandas as pd
 st.subheader("Benutzerdefinierte Daten eingeben")
 
 # Eingabe der Werte für die ersten beiden Zeilen durch den Benutzer
-mood_value = st.number_input("Wert für Mood (1-10)", min_value=1, max_value=10, value=5)
+moos_value = st.number_input("Wert für Moos (1-10)", min_value=1, max_value=10, value=5)
 stress_value = st.number_input("Wert für Stress (1-10)", min_value=1, max_value=10, value=5)
 
 # Festlegen der ersten beiden Zeilen mit den ausgewählten Werten
-data = pd.DataFrame({'Mood': ['mood_value'], 'Stress ': [stress_value]})
+data = pd.DataFrame({'Datum': ['Moos', 'Stress'], pd.Timestamp.now().date(): [moos_value, stress_value]}).T.rename(columns={0:'Wert'})
 
 # Eingabe der Anzahl der zusätzlichen Spalten durch den Benutzer
 num_cols = st.number_input("Anzahl der zusätzlichen Spalten", min_value=0, value=0)
