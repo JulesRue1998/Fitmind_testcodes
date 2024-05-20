e14d0b8705cc24ffeb4081d3f9db8a0ab012b485
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-05-21 00:10:32 +0200

    Update Fitmind_testcodes.py

--- Fitmind_testcodes.py
+++ Fitmind_testcodes.py
@@ -1,38 +1,4 @@
-import pandas as pd
-import streamlit as st
-from datetime import datetime
 
-st.subheader("Mood & Stress Tracker")
-
-# Aktuelles Datum
-current_date = datetime.now().date()
-
-# DataFrame mit dem aktuellen Datum erstellen
-data_df = pd.DataFrame(
-    {
-        "Datum": [current_date],
-        "Stress": [None],
-        "Mood": [None],
-    }
-)
-
-mood = st.text_input("Mood", key="mood")
-try:
-    mood = float(mood)  # Umwandlung des Benutzereingabewerts in eine Gleitkommazahl
-except ValueError:
-    mood = None  # Wenn die Eingabe keine gültige Zahl ist, wird None verwendet
-data_df["Mood"] = mood
-
-# Benutzereingabe für Stress (beliebige Zahl)
-stress = st.text_input("Stress", key="stress")
-try:
-    stress = float(stress)  # Umwandlung des Benutzereingabewerts in eine Gleitkommazahl
-except ValueError:
-    stress = None  # Wenn die Eingabe keine gültige Zahl ist, wird None verwendet
-data_df["Stress"] = stress
-
-# Anzeigen des DataFrames
-st.write(data_df)
 
 import pandas as pd
 import streamlit as st
