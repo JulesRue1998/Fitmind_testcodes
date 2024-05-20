4ae5cc56d4445ccc231ee57828968c7c3cb9f082
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-05-20 21:41:02 +0200

    Update Fitmind_testcodes.py

--- Fitmind_testcodes.py
+++ Fitmind_testcodes.py
@@ -42,6 +42,9 @@ plt.plot(df['Date'], df['Mood'], marker='o', label='Mood')
 plt.xlabel('Date')
 plt.ylabel('Level')
 plt.title('Stress Level and Mood Over Time')
+xticks = [df['Date'].iloc[0], pd.Timestamp.now().date()]
+plt.xticks(xticks, rotation=45)
 plt.xticks(rotation=45)
+plt.yticks(range(1, 11))  # Fixing y-axis from 1 to 10
 plt.legend()
 st.pyplot(plt)
