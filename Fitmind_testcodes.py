23dfb98d43d43708e1043d7216b214878f6acc28
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-05-20 23:05:40 +0200

    Update Fitmind_testcodes.py

--- Fitmind_testcodes.py
+++ Fitmind_testcodes.py
@@ -11,13 +11,7 @@ df = pd.DataFrame(columns=["Stimmung", "Stress"])
 # Heutiges Datum
 current_date = datetime.now().date()
 
-# Schleife zur Eingabe der Daten für jede Zeile
-for i in range(num_rows):
-    # Benutzereingabe für Stimmung und Stress für jede Zeile
-    mood = st.number_input(f"Stimmung", min_value=1, max_value=10, step=1)
-    stress = st.number_input(f"Stress", min_value=1, max_value=10, step=1)
-    # Hinzufügen der eingegebenen Daten als Zeile zum DataFrame
-    df.loc[current_date] = [mood, stress]
+
 
 import streamlit as st
 import pandas as pd
