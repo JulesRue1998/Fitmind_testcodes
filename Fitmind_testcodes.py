327329f5abeee6ac6288ea99d3d23389f608a2e0
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-05-20 23:09:36 +0200

    Update Fitmind_testcodes.py

--- Fitmind_testcodes.py
+++ Fitmind_testcodes.py
@@ -6,15 +6,15 @@ from datetime import datetime
 def add_columns(df, num_columns):
     current_date = datetime.now().date()
     for i in range(num_columns):
-        mood = st.number_input(f"Stimmung (Spalte {len(df.columns)+i+1})", min_value=1, max_value=10, step=1)
-        stress = st.number_input(f"Stress (Spalte {len(df.columns)+i+1})", min_value=1, max_value=10, step=1)
-        df[current_date] = [mood, stress]
+        mood = st.number_input(f"Spalte {len(df.columns)+i}: Mood", min_value=1, max_value=10, step=1)
+        stress = st.number_input(f"Spalte {len(df.columns)+i}: Stress", min_value=1, max_value=10, step=1)
+        df[f"Spalte {len(df.columns)+i}"] = [mood, stress]
     return df
 
 # Laden des vorhandenen DataFrames (falls vorhanden)
 @st.cache(allow_output_mutation=True)
 def load_df():
-    return pd.DataFrame(columns=["Datum"])
+    return pd.DataFrame(columns=["Mood", "Stress"])
 
 # Benutzereingabe für die Anzahl der zusätzlichen Spalten
 num_additional_columns = st.number_input("Anzahl der zusätzlichen Spalten", min_value=1, value=1, step=1)
