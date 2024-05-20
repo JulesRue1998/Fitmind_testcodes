946a55ba8d8115f0ef2fb9ff7f9b9bbbdad2e302
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-05-20 22:45:55 +0200

    Update Fitmind_testcodes.py

--- Fitmind_testcodes.py
+++ Fitmind_testcodes.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(np.random.randn(10, 5), columns=["Mood", "Stress"]))
+df = pd.DataFrame(np.random.randn(1, 5), columns=["Mood", "Stress"]))
 
 st.table(df)
