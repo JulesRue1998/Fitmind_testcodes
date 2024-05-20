0dd37145b43ac501fcd27dcc6f988cd2d9cd990a
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-05-20 21:45:21 +0200

    Update Fitmind_testcodes.py

--- Fitmind_testcodes.py
+++ Fitmind_testcodes.py
@@ -4,38 +4,30 @@ import matplotlib.pyplot as plt
 
 # Initialisierung der Daten nur beim ersten Aufruf der Seite
 if 'data' not in st.session_state:
-    st.session_state.data = {
-        'Date': [],
-        'Mood': [],
-        'Stress Level': []
-    }
+    st.session_state.data = pd.DataFrame(columns=['Date', 'Mood', 'Stress Level'])
 
 # Title und Anweisungen
 st.subheader("Mental Health")
 st.write("Wie geht es dir heute?")
-mood = st.slider("Stimmung", 1, 10, 5)
+
 # Input Widgets
-st.write("Wie gestresst warst du heute")
+mood = st.slider("Stimmung", 1, 10, 5)
 stress_level = st.slider("Stresslevel", 1, 10, 5)
 
 # Daten an die Session-Variable anhängen
 if st.button("Daten speichern"):
-    st.session_state.data['Date'].append(pd.Timestamp.now().date())
-    st.session_state.data['Mood'].append(mood)
-    st.session_state.data['Stress Level'].append(stress_level)
-
-# Umwandlung der Daten in DataFrame
-df = pd.DataFrame(st.session_state.data)
+    new_entry = {'Date': pd.Timestamp.now().date(), 'Mood': mood, 'Stress Level': stress_level}
+    st.session_state.data = st.session_state.data.append(new_entry, ignore_index=True)
 
 # Liniendiagramm
 st.subheader("Stress Level and Mood Over Time")
 plt.figure(figsize=(10, 6))
 
 # Stresslevel plotten
-plt.plot(df['Date'], df['Stress Level'], marker='o', label='Stress Level')
+plt.plot(st.session_state.data['Date'], st.session_state.data['Stress Level'], marker='o', label='Stress Level')
 
 # Stimmung plotten
-plt.plot(df['Date'], df['Mood'], marker='o', label='Mood')
+plt.plot(st.session_state.data['Date'], st.session_state.data['Mood'], marker='o', label='Mood')
 
 plt.xlabel('Date')
 plt.ylabel('Level')
@@ -43,7 +35,7 @@ plt.title('Stress Level and Mood Over Time')
 plt.yticks(range(1, 11))  # Festlegen der y-Achse von 1 bis 10
 
 # Festlegen der x-Ticks, um nur das Datum des ersten Eintrags und das aktuelle Datum anzuzeigen
-xticks = [df['Date'].iloc[0], pd.Timestamp.now().date()]
+xticks = [st.session_state.data['Date'].iloc[0], pd.Timestamp.now().date()]
 plt.xticks(xticks, rotation=45)
 
 plt.legend()
