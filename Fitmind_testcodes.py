6b00aed64362a36a97a7a0516dedfa1359ac1ab2
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-05-21 00:16:32 +0200

    Update Fitmind_testcodes.py

--- Fitmind_testcodes.py
+++ Fitmind_testcodes.py
@@ -1,5 +1,4 @@
 
-
 import pandas as pd
 import streamlit as st
 from datetime import datetime
@@ -10,18 +9,20 @@ st.subheader("Mood & Stress Tracker")
 data_df = pd.DataFrame(columns=["Datum", "Mood", "Stress"])
 
 # Funktion zum Hinzufügen einer neuen Zeile
-def add_row():
+def add_row(df):
     # Überprüfen, ob bereits ein Eintrag für das heutige Datum vorhanden ist
-    today_entries = data_df[data_df["Datum"] == datetime.now().date()]
+    today_entries = df[df["Datum"] == datetime.now().date()]
     if len(today_entries) == 0:  # Wenn kein Eintrag vorhanden ist, eine neue Zeile hinzufügen
         new_row = {"Datum": datetime.now().date(), "Mood": None, "Stress": None}
-        data_df.loc[len(data_df)] = new_row
+        df = df.append(new_row, ignore_index=True)  # Neue Zeile hinzufügen
+        return df
     else:
         st.warning("Eintrag für heute existiert bereits.")
+        return df
 
 # Schaltfläche zum Hinzufügen einer neuen Zeile
 if st.button("Neue Zeile hinzufügen"):
-    add_row()
+    data_df = add_row(data_df)
 
 # Benutzereingabe für Mood (beliebige Zahl)
 mood = st.text_input("Mood", key="mood")
